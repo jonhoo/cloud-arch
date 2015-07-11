8ac8e64221c45ba0ef6bfdf209540a4d85bb8ff4
Jon Gjengset <jon@thesquareplanet.com>
2015-07-11 19:03:42 -0400

    Compile in cloud-utils so we get growpart

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,28 @@ sudo date > /dev/null
 
 sec "Creating bootstrap image"
 
-msg "Checking that we have Arch's install scripts"
+msg "Checking that we have necessary build tools"
 pacman -Qi arch-install-scripts >/dev/null || sudo pacman -S arch-install-scripts # for pacstrap
+pacman -Qi pacaur >/dev/null || yaourt -S pacaur
 
 tmp=$(mktemp -d -t arch-cloud-bootstrap.XXXXXXXXXX)
 tmp=$(readlink -f "$tmp")
 rm -f bootstrapped.raw
 ./mount.sh "bootstrapped.raw" "$tmp"
+
+msg "Building cloud-utils"
+# we do this locally so we don't have to pull in all of base-devel inside the VM
+bd=$(mktemp -d -t build_cloud-utils.XXXXXXXXXX)
+bd=$(readlink -f "$bd")
+pwd=$(pwd)
+cd "$bd"
+msg2 "Building with pacaur"
+# specifying AUR dependencies explicitly in case they're already installed on
+# the host
+env "BUILDDIR=$bd" pacaur --noconfirm --noedit --rebuild -f -m cloud-utils-bzr euca2ools python2-requestbuilder
+find . -type f -name '*.pkg.tar.xz' -exec sudo cp {} "$tmp" \;
+cd "$pwd"
+rm -rf "$bd"
 
 msg "Installing packages"
 sudo pacstrap -c "$tmp" base \
@@ -32,5 +47,9 @@ sudo pacstrap -c "$tmp" base \
 	#zsh fish \
 	#rxvt-unicode-terminfo \
 	#emacs (draws in gtk3!) \
+
+msg "Install cloud-utils"
+sudo arch-chroot "$tmp" find / -maxdepth 1 -type f -name '*.pkg.tar.xz' | xargs sudo arch-chroot "$tmp" pacman --noconfirm -U
+sudo arch-chroot "$tmp" find / -maxdepth 1 -type f -name '*.pkg.tar.xz' -exec rm {} \;
 
 ./unmount.sh "$tmp"
